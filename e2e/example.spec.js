770d40aa765f3e32c5ee506d0408c24625f3cc62
Almir Reis <falecomalmir@gmail.com>
2025-06-29 01:00:48 -0300

    ci: correcao

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -1,35 +1,9 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test.describe('user rides', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-  });
-
-  test('user should be able to ride', async ({ page }) => {
-    await page.getByRole('link', { name: 'Choose Roba Swings' }).click();
-    await page.getByLabel('Amount of people').selectOption('2');
-    await page.getByRole('button', { name: 'Next' }).click();
-    
-
-     await expect(page).toHaveURL(/.*#ride\/swings/);
-  });
-
-  test('user above height should not be allowed', async ({ page }) => {
-    await page.getByRole('link', { name: 'Choose Robo Coaster Of Doom' }).click();
-    await page.getByLabel('Amount of people').selectOption('1');
-    await page.getByLabel('Height for person').click();
-    await page.getByLabel('Height for person').fill('139');
-    await page.getByRole('button', { name: 'Next' }).click();
-    await expect(page.getByText('Person 1 is too short for this ride')).toBeVisible();
-  });
-
-  test('user with minimum height should be allowed', async ({ page }) => {
-    await page.getByRole('link', { name: 'Choose Robo Coaster Of Doom' }).click();
-    await page.getByLabel('Amount of people').selectOption('1');
-    await page.getByLabel('Height for person').click();
-    await page.getByLabel('Height for person').fill('141');
-    await page.getByRole('button', { name: 'Next' }).click();
-    await expect(page.getByText('1 person (>= 141 cm) for the')).toBeVisible();
+test.describe('Página inicial', () => {
+  test('Deve carregar a página com sucesso', async ({ page }) => {
+    await page.goto('http://localhost:3002');
+    await expect(page.locator('h1')).toHaveText('Hello, Pipeline!');
   });
 });
