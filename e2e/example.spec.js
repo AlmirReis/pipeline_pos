1f335ba4e51b9a48a3ab79ce9a22486c5c03a595
Almir Reis <falecomalmir@gmail.com>
2025-06-29 00:48:08 -0300

    ci: correcao

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Página inicial', () => {
   test('Deve carregar a página com sucesso', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+    await page.goto('http://localhost:3002');
     await expect(page.locator('h1')).toHaveText('Hello, Pipeline!');
   });
 });
